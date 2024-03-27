c88ef735c4984ffa088ec29e9bb2e0eb78294a77
SCAIME\akhiari <akhiari@scaime.com>
2024-03-27 12:46:08 +0100

    premier push portfolio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="Welcome to Your Vue.js "/>
+    <router-link to="/sectionhome">Section</router-link>
   </div>
 </template>
 
